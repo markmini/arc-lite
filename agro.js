531273c8f90df7dd5736e0b30bbca44ddf6ac326
markmini <105717497+markmini@users.noreply.github.com>
2024-07-22 02:34:32 +0100

    Update agro.js

--- agro.js
+++ agro.js
@@ -1,14 +1,1 @@
-document.getElementById('contact-btn1').addEventListener('click', function() {
-    window.location.href = 'contacts.html';
-  });
-  document.getElementById('learn-btn').addEventListener('click', function() {
-    window.location.href = 'services.html';
-  });  
-  
-document.getElementById('toggle-btn').addEventListener('click', function() {
-    var sidebar = document.getElementById('sidebar');
-    var content = document.getElementById('content');
-    sidebar.classList.toggle('hidden');
-    content.classList.toggle('full-width');
-  });
-  
+
